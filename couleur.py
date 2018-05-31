bda8387210da4bf045e10e733073966459fb38da
Calioups <38938563+Calioups@users.noreply.github.com>
2018-05-31 14:28:24 +0200

    Add files via upload

--- couleur.py
+++ couleur.py
@@ -9,17 +9,21 @@ from PIL import Image as img
 import numpy as np
 import copy
 
-#im1=img.open("image1")
-#im2=img.open("image2")
-#
-#data1=list(im1.getdata())
-#data2=list(im2.getdata())
+
 
 def cherchevert(data,r,g,b,t):  #data : liste des coefficients rgb des pixels r,g,g : coefficients correspondant à la couleur de l'objet cherché t : marge d'erreur
     s=[]
     for i in range(len(data)):  #si un pixel est d'une couleur semblable à la marge près de la couleur cible on enregistre la position de ce pixel
-        if (r-t)<data[i][0] & data[i][0]<(r+t) & (g-t)<data[i][1] & data[i][1]<g+t & (b-t)<data[i][2] & data[i][2]<(b+t):
-            s.append(i)
+        if data[1][3]>100:
+            a=(r-t)<data[i][0]
+            b=data[i][0]<(r+t)
+            c=(g-t)<data[i][1]
+            d=data[i][1]<g+t
+            e=(b-t)<data[i][2] 
+            f=data[i][2]<(b+t)
+            g=a & b & c & d & e & f
+            if g:
+                s.append(i)         
     return s #retour : liste des positions
 
 def pos(z,x):           #z : position d'un pixel    x: largeur de l'image
@@ -51,12 +55,6 @@ def posmoyen(l):    # l : liste de positions de pixels dans une représentation 
         y+=l[i][1]
     return (x/len(l),y/len(l)) # retour : (x,y) position moyenne des pixels
     
-def suivi(data,r,g,b,t,x,y):     #data : liste des coefficients rgb des pixels r,g,g : coefficients correspondant à la couleur de l'objet cherché t : marge d'erreur x,y : position de l'image
-    d=cherchevert(data,r,g,b,t)  #on regarde où sont les pixels proches de la couleur cible
-    listeverte=reduction(d,x)
-    position=listepos(listeverte)   
-    return (posmoyen(position),listeverte) # retour (xmoy,ymoy),listepixelscibles 
-    
     
 def retour(data,position): # data : liste des valeurs des pixels   position : liste de la position des pixels ayant été définis comme faisant partie de la cible
     r1=0
@@ -64,7 +62,7 @@ def retour(data,position): # data : liste des valeurs des pixels   position : li
     b1=0
     for i in position:
         r1+=data[i][0]
-        g1+=data[i][1]
+        g1+=data[i][1]      #calcule les coefficients moyens des pixels cibles
         b1+=data[i][2]
     n=len(position)
     r1=r1/n
@@ -96,43 +94,27 @@ def groupe(l,i,x):
     return s
     
 def reduction(l,x):
-    s=[]    
-    for a in l:         #on enlève les pixels isolés pour affiner
-        if comptevoisin(l,a,x)>3:
-            s.append(a)
-    if len(l)<1000:
-        q=copy.deepcopy(s)
+        q=copy.deepcopy(l)
         w=[]
         for a in q:
+            w.append(q.pop(q.index(a)))
             w=w+groupe(q,a,x)
         v=[]
         for a in w:
             v.append(len(a))
-        return w[w.index(max(w))]
-    else :
-        return s
+        return v[v.index(max(v))]
     
+def suivi(data,r,g,b,t,x,y):     #data : liste des coefficients rgb des pixels r,g,g : coefficients correspondant à la couleur de l'objet cherché t : marge d'erreur x,y : position de l'image
+    d=cherchevert(data,r,g,b,t)  #on regarde où sont les pixels proches de la couleur cible
+    listeverte=reduction(d,x)
+    position=listepos(listeverte)   
+    return (posmoyen(position),listeverte) # retour (xmoy,ymoy),listepixelscibles  
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+im1=img.open("C:/Users/SIMPLYCASH/Pictures/Saved Pictures/image0.png")
+im2=img.open("C:/Users/SIMPLYCASH/Pictures/Saved Pictures/image1.png")
+
+data1=list(im1.getdata())
+data2=list(im2.getdata())
+
+
+k2=suivi(data1,160,150,60,100,1680,1050)
